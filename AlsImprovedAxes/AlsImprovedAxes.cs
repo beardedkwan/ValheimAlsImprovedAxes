9c999e38c3b40b923a20c957ffd897034b4f05c8
beardedkwan <amar536@protonmail.com>
2023-09-07 09:23:09 -0600

    Incremented version

--- AlsImprovedAxes/AlsImprovedAxes.cs
+++ AlsImprovedAxes/AlsImprovedAxes.cs
@@ -16,7 +16,7 @@ namespace AlsImprovedAxes
     {
         public const string Name = "Al's Improved Axes";
         public const string Guid = "beardedkwan.AlsImprovedAxes";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
     }
     
     public class AlsImprovedAxesConfig
